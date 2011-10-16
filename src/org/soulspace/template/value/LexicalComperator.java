ab80e4bc5f71d490fcecb48686366be57dc03d0a
Ludger Solbach <soulman@soulspace.org>
2011-10-16 22:55:04 +0000

    made LexicalComperator serializable

--- src/org/soulspace/template/value/LexicalComperator.java
+++ src/org/soulspace/template/value/LexicalComperator.java
@@ -1,8 +1,11 @@
 package org.soulspace.template.value;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class LexicalComperator implements Comparator<Value> {
+public class LexicalComperator implements Comparator<Value>, Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public int compare(Value arg0, Value arg1) {
 		return arg0.asString().compareTo(arg1.asString());
