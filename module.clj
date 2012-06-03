d26a89cfe613e37d54a238355c9c8ef078e4d323
Ludger Solbach <soulman@soulspace.org>
2012-06-03 16:41:10 +0000

    added baumeister support

---
+++ module.clj
@@ -0,0 +1,10 @@
+[
+ :name "TemplateEngine"
+ :type "library"
+ :version "1.0.0rc7"
+ :description "General purpose template engine"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "archive" "publish"]
+ :dependencies [["apache-oro" "jakarta-oro" "2.0.8"]
+                ["org.junit" "junit" "3.8.1" "dev"]]
+ ]
