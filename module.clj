3a6b8cc64aeb472e841173a96feca26cc93fe084
Ludger Solbach <soulman@soulspace.org>
2013-03-07 01:02:30 +0100

    renamed project, updated dependencies, fixed ant build

--- module.clj
+++ module.clj
@@ -1,6 +1,6 @@
 [
  :name "TemplateEngineTask"
- :project "org.soulspace.modelling"
+ :project "org.soulspace.template"
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
  :type "framework"
@@ -8,8 +8,8 @@
  :description "Ant task for the use of the template engine in ant build files."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace" "TemplateEngine" "1.0.0rc7"]
-                ["org.soulspace" "JavaUtilLibrary" "0.2.0"]
+ :dependencies [["org.soulspace.template" "TemplateEngine" "1.0.0"]
+                ["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]
                 ["oro" "oro" "2.0.8"]
                 ["org.apache.ant" "ant" "1.8.3" "dev"]
                 ["junit" "junit" "3.8.1" "dev"]]
