f5cfd90e6b2bdf9160d8c724af21ab2f5e5e654f
Ludger Solbach <soulman@soulspace.org>
2013-07-23 00:10:40 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,5 +1,5 @@
 [
- :name "TemplateEngine"
+ :module "TemplateEngine"
  :project "org.soulspace.template"
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
@@ -7,11 +7,11 @@
  :type "component"
  :description "General purpose template engine"
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "deps" "java" "junit" "package"]
- :dependencies [["oro" "oro" "2.0.8"]
-                ["junit" "junit" "3.8.1" "dev"]
-                ["com.clarkware" "junitperf" "1.9.1" "dev"]
-                ["org.fitnesse" "fitnesse" "20060719" "dev"]
-                ["org.fitnesse" "fitnesse" "20060719" "dev" "fitlibrary" ]
+ :plugins ["global" "sdeps" "java" "junit" "package"]
+ :dependencies [[["oro" "oro" "2.0.8"]]
+                [["junit" "junit" "3.8.1"] "dev"]
+                [["com.clarkware" "junitperf" "1.9.1"] "dev"]
+                [["org.fitnesse" "fitnesse" "20060719"] "dev"]
+                [["org.fitnesse" "fitnesse" "20060719" "fitlibrary"] "dev"]
                 ]
  ]
