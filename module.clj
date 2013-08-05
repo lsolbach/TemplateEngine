9f43d6d206af171434d9c76c365bc44fd93a883a
Ludger Solbach <soulman@soulspace.org>
2013-08-05 06:47:44 +0200

    bumped version to 1.0.1, updated TemplateEngine to 1.0.1

--- module.clj
+++ module.clj
@@ -4,11 +4,11 @@
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
  :type :framework
- :version "0.1.0"
+ :version "1.0.1"
  :description "Ant task for the use of the template engine in ant build files."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "package"]
- :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.0"]]
+ :plugins ["global" "dependencies" "java" "package" "release"]
+ :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.1"]]
                 [["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]]
                 [["org.apache.ant" "ant" "1.8.3"] :dev]
                 [["junit" "junit" "3.8.1"] :dev]]
