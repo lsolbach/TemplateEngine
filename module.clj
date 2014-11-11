031954548863482e1a0a0819cab3aeadfa8842fb
soulman <soulman@pluto>
2014-11-11 10:54:37 +0100

    updated project files

--- module.clj
+++ module.clj
@@ -7,9 +7,13 @@
  :version "1.0.1"
  :description "Ant task for the use of the template engine in ant build files."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "package" "release"]
- :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.1"]]
-                [["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]]
-                [["org.apache.ant" "ant" "1.8.3"] :dev]
-                [["junit" "junit" "3.8.1"] :dev]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.soulspace.template/TemplateEngine, 1.0.2"]
+                ["org.soulspace.common/JavaUtilLibrary, 0.3.0"]
+                ["org.apache.ant/ant, 1.8.3" :dev]
+                ["junit/junit, 3.8.1" :dev]]
  ]
